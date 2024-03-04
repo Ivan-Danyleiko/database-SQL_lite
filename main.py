90ba1b8f01d733fb1a7fd5b19fe95ba02f2ca340
Ivan Danyleiko <147879140+Ivan-Danyleiko@users.noreply.github.com>
2024-03-04 12:44:18 +0200

    main.py

--- main.py
+++ main.py
@@ -4,24 +4,20 @@ import random
 
 fake = Faker()
 
-# Підключення до бази даних
 conn = sqlite3.connect('your_database_name.db')
 cursor = conn.cursor()
 
-# Заповнення таблиці груп
 for i in range(3):
     group_name = fake.word()
     cursor.execute("INSERT INTO groups (group_name) VALUES (?)", (group_name,))
     conn.commit()
 
-# Заповнення таблиці викладачів
 for i in range(3):
     teacher_first_name = fake.first_name()
     teacher_last_name = fake.last_name()
     cursor.execute("INSERT INTO teachers (first_name, last_name) VALUES (?, ?)", (teacher_first_name, teacher_last_name))
     conn.commit()
 
-# Заповнення таблиці студентів
 for i in range(30):
     first_name = fake.first_name()
     last_name = fake.last_name()
@@ -29,14 +25,12 @@ for i in range(30):
     cursor.execute("INSERT INTO students (first_name, last_name, group_id) VALUES (?, ?, ?)", (first_name, last_name, group_id))
     conn.commit()
 
-# Заповнення таблиці предметів
 subjects = ["Math", "Physics", "Chemistry", "History", "English"]
 for subject in subjects:
     teacher_id = random.randint(1, 3)
     cursor.execute("INSERT INTO subjects (subject_name, teacher_id) VALUES (?, ?)", (subject, teacher_id))
     conn.commit()
 
-# Заповнення таблиці оцінок
 for student_id in range(1, 31):
     for subject_id in range(1, 6):
         grade = random.randint(1, 10)
@@ -44,5 +38,4 @@ for student_id in range(1, 31):
         cursor.execute("INSERT INTO grades (student_id, subject_id, grade, exam_date) VALUES (?, ?, ?, ?)", (student_id, subject_id, grade, exam_date))
         conn.commit()
 
-# Закриття підключення до бази даних
 conn.close()
